5e3a9bb57360bc39bd2a024f7b103a840284c44e
mayhemph <vserhiy@hotmail.com>
2023-09-21 00:23:32 +0300

    another

--- components/Vdspdptemplatebreakdown.client.tsx
+++ components/Vdspdptemplatebreakdown.client.tsx
@@ -4,22 +4,20 @@ import { Runtime, Inspector } from "@observablehq/runtime";
 import notebook from "@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown";
 
 function Vdspdptemplatebreakdown() {
-  const chart6Ref = useRef<HTMLDivElement | null>(null); // Corrected line
+  const chart6Ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const runtime = new Runtime();
-    runtime.module(notebook, (name: string) => {
-      if (name === "chart6") return new Inspector(chart6Ref.current);
+    runtime.module(notebook, (name) => {
+      if (name === "chart6" && chart6Ref.current) {
+        return new Inspector(chart6Ref.current);
+      }
     });
     return () => runtime.dispose();
   }, []);
 
-  return (
-    <>
-      <div ref={chart6Ref} />
-      <p>Credit: <a href="https://observablehq.com/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@470">Untitled by Sergiy Vasyletskyy&#039;s Workspace</a></p>
-    </>
-  );
+  return <div ref={chart6Ref}></div>;
 }
 
 export default Vdspdptemplatebreakdown;
+
