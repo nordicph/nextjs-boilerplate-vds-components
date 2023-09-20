1edb54f05ccb12534bd9e17303ff85d4b6a7554e
mayhemph <vserhiy@hotmail.com>
2023-09-21 00:49:03 +0300

    package

--- components/ObservableChart.js
+++ components/ObservableChart.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Runtime, Inspector } from "@observablehq/runtime";
-import notebook from "./obs_code/obs.json";
+import notebook from "./obs_code/package.json";
 
 function ObservableChart() {
   const chartRef = useRef();
