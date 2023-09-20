49d528d32e66b56ca84cdb2e5697df4abac31550
mayhemph <vserhiy@hotmail.com>
2023-09-21 00:36:36 +0300

    iframe

---
+++ components/ObservableChart.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ObservableChart() {
+  return (
+    <div>
+      <iframe
+        src="https://observablehq.com/embed/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown?cell=chart6"
+        frameBorder="0"
+        allowFullScreen
+        width="100%"
+        height="400px"
+      ></iframe>
+    </div>
+  );
+}
+
+export default ObservableChart;
--- components/Vdspdptemplatebreakdown.client.tsx
+++ components/Vdspdptemplatebreakdown.client.tsx
@@ -1,23 +1,44 @@
 'use client'
-import React, { useRef, useEffect } from "react";
-import { Runtime, Inspector } from "@observablehq/runtime";
-import notebook from "@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown";
+import React, { useEffect } from "react";
 
 function Vdspdptemplatebreakdown() {
-  const chart6Ref = useRef<HTMLDivElement | null>(null);
-
   useEffect(() => {
-    const runtime = new Runtime();
-    runtime.module(notebook, (name: string) => { // Explicitly specify the type
-      if (name === "chart6" && chart6Ref.current) {
-        return new Inspector(chart6Ref.current);
-      }
-    });
-    return () => runtime.dispose();
+    const script = document.createElement("script");
+    script.src =
+      "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+    script.type = "module";
+
+    script.onload = () => {
+      import("https://api.observablehq.com/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@470.js?v=3").then((module) => {
+        const { Runtime, Inspector } = module.default;
+        const define = module.define;
+
+        new Runtime().module(define, (name: string) => {
+          if (name === "chart6") {
+            new Inspector(document.querySelector("#observablehq-chart6-67bdaa66"));
+          }
+        });
+      });
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
 
-  return <div ref={chart6Ref}></div>;
+  return (
+    <>
+      <div id="observablehq-chart6-67bdaa66"></div>
+      <p>
+        Credit:{" "}
+        <a href="https://observablehq.com/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@470">
+          Untitled by Sergiy Vasyletskyy&#039;s Workspace
+        </a>
+      </p>
+    </>
+  );
 }
 
 export default Vdspdptemplatebreakdown;
-
