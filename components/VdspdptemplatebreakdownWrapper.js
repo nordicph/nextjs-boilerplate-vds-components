652ccadad8af6bc290b01455b222c995fd9fffe2
mayhemph <vserhiy@hotmail.com>
2023-09-20 23:57:54 +0300

    final

--- components/VdspdptemplatebreakdownWrapper.js
+++ components/VdspdptemplatebreakdownWrapper.js
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 
 const Vdspdptemplatebreakdown = dynamic(
   () => import('./Vdspdptemplatebreakdown.client.tsx'),
-  { ssr: false, loading: () => <p>Loading...</p> }
+  { ssr: false }
 );
 
 function VdspdptemplatebreakdownWrapper() {
