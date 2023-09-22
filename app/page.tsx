7c6aff5706e451a59d947f80b79d53d64e4af55c
mayhemph <vserhiy@hotmail.com>
2023-09-22 13:07:52 +0300

    embedly

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from "react";
-import EmbedlyIframe from "./EmbedlyIframe";
+import EmbedlyIframe from "../components/EmbedlyIframe.js";
 
 export default function Home() {
   return (
