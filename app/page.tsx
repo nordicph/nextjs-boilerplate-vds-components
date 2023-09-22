0d7d81e82e3117d3a7318871ddc845179d014083
mayhemph <vserhiy@hotmail.com>
2023-09-22 10:42:06 +0300

    adjust

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <p className="custom-text fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Vital DS Standard Website. Taxonomy, Genealogy, and Delivery Timelines &nbsp;
+        Vital DS Standard Website. &nbsp;
         <code className="font-mono font-bold"></code>
       </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -31,7 +31,7 @@ export default function Home() {
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
         style={{ marginTop: '10px', marginBottom: '10px' }}
       >
-        Vital DS Standard Website Delivery Timeline   &nbsp;
+        Vital DS Standard Website Delivery Timeline.   &nbsp;
       </p>
 
       <iframe
@@ -45,7 +45,7 @@ export default function Home() {
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
         style={{ marginTop: '10px', marginBottom: '10px' }}
       >
-        Vital DS Standard Website Taxonomy    &nbsp;
+        Vital DS Standard Website Taxonomy.    &nbsp;
       </p>
       <iframe
       style={{ backgroundColor: "white" }}
