5f6fe50e5e66d14f45ac8d68e163ceb10abfdbd4
mayhemph <vserhiy@hotmail.com>
2023-09-22 16:18:35 +0300

    16

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
       width="75%"
       height="930"
       frameBorder="0"
-      src="https://observablehq.com/embed/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@504?cells=chart"
+      src="https://observablehq.com/embed/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@508?cells=chart"
       scrolling="no"
       ></iframe>
 
