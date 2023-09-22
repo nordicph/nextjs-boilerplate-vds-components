e5a77f84f6f5760283383fa03f39af375a1d2a76
mayhemph <vserhiy@hotmail.com>
2023-09-22 10:04:28 +0300

    k

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/Kenvue.svg"
+              src="/kenvue.svg"
               alt="Kenvue Logo"
               width={100}
               height={24}
