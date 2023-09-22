8afc4688a358f8f59a5cfabb49ba5cc78706be6c
mayhemph <vserhiy@hotmail.com>
2023-09-22 10:57:34 +0300

    600

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
               width={100}
               height={24}
+              
               priority
             />
           </a>
