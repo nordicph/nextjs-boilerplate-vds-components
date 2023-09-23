89cf85f56f5b939a66ca3768b32c54c70cb735d4
mayhemph <vserhiy@hotmail.com>
2023-09-23 16:37:28 +0300

    1

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       width="75%"
       height="640"
       frameBorder="0"
-      src="https://observablehq.com/embed/7831f502b6563196@314?cells=StandardWebsiteTimeline2"
+      src="https://observablehq.com/embed/7831f502b6563196@317?cells=StandardWebsiteTimeline2"
       ></iframe>
       <p
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
