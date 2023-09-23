45f7d3ca79c3155a55a5ffef47fbaa1fbfb68e6c
mayhemph <vserhiy@hotmail.com>
2023-09-23 15:24:06 +0300

    expanded

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       width="75%"
       height="640"
       frameBorder="0"
-      src="https://observablehq.com/embed/7831f502b6563196@277?cells=StandardWebsiteTimeline2"
+      src="https://observablehq.com/embed/7831f502b6563196@278?cells=StandardWebsiteTimeline2"
       ></iframe>
       <p
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
@@ -71,7 +71,7 @@ export default function Home() {
       width="75%"
       height="1200"
       frameBorder="0"
-      src="https://observablehq.com/embed/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@612?cells=chart6"
+      src="https://observablehq.com/embed/@sergiy-vasyletskyy-ws/vds_pdp_template_breakdown@617?cells=chart6"
       scrolling="no"
       ></iframe>
       <div className="divider"></div>
