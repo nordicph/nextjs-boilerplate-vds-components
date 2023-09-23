7f401bf0975974132b6bc3e0d914887d75ef88fa
mayhemph <vserhiy@hotmail.com>
2023-09-23 16:57:19 +0300

    1080

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
       <iframe
       style={{ backgroundColor: "white", borderBottom: "1px solid blue" }}
       width="75%"
-      height="1440"
+      height="1080"
       frameBorder="0"
       src="https://observablehq.com/embed/7533c984eedc1bc7@547?cells=chart_links"
       scrolling="no"
