8ecd1a54da26c6c95fa431078a9cddfb416ef3b7
mayhemph <vserhiy@hotmail.com>
2023-09-24 10:02:50 +0300

    comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from "react";
+import ObservableChart from "../components/ObservableChart";
 import { useEffect, useRef, useState } from 'react';
 import $ from 'jquery';
 
