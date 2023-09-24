0502a996b5453fa719f38f7bf46b6ad54fe92bc2
mayhemph <vserhiy@hotmail.com>
2023-09-24 14:18:20 +0300

    winter tree

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
         style={{ marginTop: '10px', marginBottom: '10px' }}
       >
-        Vital DS Standard Website Component Genealogy | Focused &nbsp;
+        Vital DS Standard Website Component Genealogy | In-Focus | Interactive &nbsp;
       </p>
       <iframe
       className="observable-iframe"
@@ -129,7 +129,7 @@ export default function Home() {
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
         style={{ marginTop: '10px', marginBottom: '10px' }}
       >
-        Vital DS Standard Website Component Genealogy | Tree &nbsp;
+        Vital DS Standard Website Component Genealogy | Winter Tree | Interactive &nbsp;
       </p>
       <iframe
       className="observable-iframe"
