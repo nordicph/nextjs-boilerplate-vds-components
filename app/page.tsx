d69a0ec440144550fce645ead9b19bd785aea2ac
mayhemph <vserhiy@hotmail.com>
2023-09-24 18:20:47 +0300

    underscore

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
       style={{ backgroundColor: "#FFFFFF", borderBottom: "1px solid blue", border: "none", overflow: "hidden" }}
       width="75%"
       height="640"
-      src="https://observablehq.com/embed/7831f502b6563196@317?cells=StandardWebsiteTimeline2"
+      src="https://observablehq.com/embed/7831f502b6563196@328?cells=StandardWebsiteTimeline2"
       ></iframe>
       <p
         className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold"
