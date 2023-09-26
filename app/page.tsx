d9ca58e25f725677f0f6c8d5acbdeab5702f2073
mayhemph <vserhiy@hotmail.com>
2023-09-26 14:21:56 +0300

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,6 @@ export default function Home() {
         }
       }
     };
-    
 
     if (openMenuButton) {
       openMenuButton.addEventListener('click', handleButtonClick);
@@ -120,27 +119,28 @@ export default function Home() {
             </nav>
         </header>
  
-      <p className="custom-text flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
+        <p className="custom-text flex w-full lg:w-screen justify-start ml-0 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
         Vital DS Standard Website. &nbsp;
         <code className="font-mono font-bold"></code>
-      </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/kenvue.svg"
-              alt="Kenvue Logo"
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              width={100}
-              height={24}
-            />
-          </a>
+        </p>
+        <div className="fixed bottom-0 right-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                By{' '}
+                <Image
+                    src="/kenvue.svg"
+                    alt="Kenvue Logo"
+                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                    width={100}
+                    height={24}
+                />
+            </a>
         </div>
+
       </div>
 
       <IFrameWithDescription 
