9f5f887a2021adbb4bcf2fe614ddd48bbcda04de
mayhemph <vserhiy@hotmail.com>
2023-09-26 19:58:04 +0300

    bc

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,17 @@ interface IFrameWithDescriptionProps {
   description: string;
 }
 
-
-
 function IFrameWithDescription({ id, src, backgroundColor, height, description }: IFrameWithDescriptionProps) {
   return (
     <div id={id}>
     <>
-      <div className="divider"></div>
       <p className="mt-2.5 mb-2.5 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold">
         {description}
       </p>
       <iframe
         className="observable-iframe"
         style={{ backgroundColor: backgroundColor, borderBottom: "1px solid blue", border: "none", overflow: "hidden" }}
-        width="75%"
+        width="100%"
         height={height}
         src={src}
       ></iframe>
@@ -102,7 +99,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="wrapper">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
           {/* Header with navigation */}
           <header id="Vital DS Standard Website Header" className="">
             <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 714.6 401.1">
@@ -120,31 +116,8 @@ export default function Home() {
                 </button>
             </nav>
         </header>
- 
-      <p className="custom-text flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
-        Vital DS Standard Website. &nbsp;
-        <code className="font-mono font-bold"></code>
-      </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/kenvue.svg"
-              alt="Kenvue Logo"
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              width={100}
-              height={24}
-            />
-          </a>
-        </div>
       </div>
-
-      <div className="divider"></div>
+      
       <IFrameWithDescription 
         id="timeline" 
         src="https://observablehq.com/embed/1cde9c5aecae0048@71?cells=StandardWebsiteTimeline2"
