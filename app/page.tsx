8694399aed85d9b687dcc0562e9901e238d84745
mayhemph <vserhiy@hotmail.com>
2023-09-26 18:39:44 +0300

    menuopen

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
     
   }, []);
   return (
-<main className="flex min-h-screen flex-col items-center justify-between pt-100 p-24" ref={pageRef}>
+<main className="menuopen flex min-h-screen flex-col items-center justify-between pt-100 p-24" ref={pageRef}>
     <div className="wrapper">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
