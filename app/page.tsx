a39d3c7fc9b7834bb339389187f2f2156cf38fde
mayhemph <vserhiy@hotmail.com>
2023-09-26 14:24:51 +0300

    border

--- app/page.tsx
+++ app/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
             </nav>
         </header>
  
-        <p className="custom-text flex w-full lg:w-screen justify-start ml-0 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
+        <p className="custom-text flex w-full lg:w-screen justify-start ml-0 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-600 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
         Vital DS Standard Website. &nbsp;
         <code className="font-mono font-bold"></code>
         </p>
