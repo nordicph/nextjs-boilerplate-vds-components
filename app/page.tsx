bba5d227785d1e65016ba31ac1b60caea6b8efea
mayhemph <vserhiy@hotmail.com>
2023-09-26 13:58:43 +0300

    headr

--- app/page.tsx
+++ app/page.tsx
@@ -100,6 +100,7 @@ export default function Home() {
   }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="wrapper">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
           {/* Header with navigation */}
@@ -119,7 +120,7 @@ export default function Home() {
                 </button>
             </nav>
         </header>
-      <div className="wrapper">
+ 
       <p className="custom-text flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold">
         Vital DS Standard Website. &nbsp;
         <code className="font-mono font-bold"></code>
