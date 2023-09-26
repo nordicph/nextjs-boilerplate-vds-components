8f0abb4365c78c405410ce70878cde0ba7b9b6d8
mayhemph <vserhiy@hotmail.com>
2023-09-26 14:25:56 +0300

    w/o logo

--- app/page.tsx
+++ app/page.tsx
@@ -123,24 +123,6 @@ export default function Home() {
         Vital DS Standard Website. &nbsp;
         <code className="font-mono font-bold"></code>
         </p>
-        <div className="fixed bottom-0 right-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                By{' '}
-                <Image
-                    src="/kenvue.svg"
-                    alt="Kenvue Logo"
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    width={100}
-                    height={24}
-                />
-            </a>
-        </div>
-
       </div>
 
       <IFrameWithDescription 
