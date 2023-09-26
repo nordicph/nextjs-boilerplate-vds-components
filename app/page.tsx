381ec8802e614515697611b9e76fdf3908f6f204
mayhemph <vserhiy@hotmail.com>
2023-09-26 14:08:44 +0300

    100

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,8 @@ export default function Home() {
     };
   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24 p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-100 p-24">
+
       <div className="wrapper">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
