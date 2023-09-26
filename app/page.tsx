54dfd143f7b4cace3340960857029e35d61f28d8
mayhemph <vserhiy@hotmail.com>
2023-09-26 14:05:10 +0300

    no divider

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ function IFrameWithDescription({ id, src, backgroundColor, height, description }
   return (
     <div id={id}>
     <>
-      <div className="divider"></div>
       <p className="mt-2.5 mb-2.5 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-mono font-bold">
         {description}
       </p>
